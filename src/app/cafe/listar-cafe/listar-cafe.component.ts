a01caa3df93f2e1dc79c28883d10dd2496ff3f0e
Andrea Villamizar <af.villamizarr1@uniandes.edu.co>
2024-04-27 07:29:57 -0500

    Se finaliza punto 2.

--- src/app/cafe/listar-cafe/listar-cafe.component.ts
+++ src/app/cafe/listar-cafe/listar-cafe.component.ts
@@ -9,17 +9,33 @@ import { CafeService } from '../cafe.service';
 })
 export class ListarCafeComponent implements OnInit {
   cafes: Array<Cafe> = [];
+  cafeOrigen:number = 0;
+  cafeBlend:number = 0;
 
   constructor(private cafeService: CafeService) { }
 
   getCafes(): void {
     this.cafeService.getCafes().subscribe((cafes) => {
       this.cafes = cafes;
+      this.cafeOrigen = contarCafeDeOrigen(this.cafes);
+      this.cafeBlend = contarCafeBlend(this.cafes);
     });
   }
 
+  
+  
   ngOnInit() {
     this.getCafes();
   }
+  
+}
 
+function contarCafeDeOrigen(cafes: any[]): number {
+  const cafeDeOrigen = cafes.filter(item => item.tipo === "Café de Origen");
+  return cafeDeOrigen.length;
+}
+
+function contarCafeBlend(cafes: any[]): number {
+  const cafeDeOrigen = cafes.filter(item => item.tipo === "Blend");
+  return cafeDeOrigen.length;
 }
